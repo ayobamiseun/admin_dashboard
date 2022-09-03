bc31f4d2ad0acbc255967a7f8f2a40427d2fd9a2
Adegoke Ayobami <adegokeayobamiseun@gmail.com>
2022-09-03 18:31:02 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-	define('SQL_USER', 'supemzih_apps');
-	define('DB', 'supemzih_apps');
-	define('SQL_PASS', 'Brain4us@7788');
+	define('SQL_USER', '');
+	define('DB', '');
+	define('SQL_PASS', '');
 	define('SQL_DB', 'localhost');
 	//$con = mysqli_connect(SQL_DB, SQL_USER, SQL_PASS, DB) or die(mysqli_error());
 	$con = mysqli_connect(SQL_DB, SQL_USER, SQL_PASS, DB);
